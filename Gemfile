27fe6de1b7fcec4ca805ba7e325333dfdc1472d5
Sebastian Hermida <sebastian@path11.com>
2012-10-07 18:05:48 +0200

    Adding compass/susy to the configuration

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source :rubygems
 
 gem 'coffee-script'
+gem 'compass'
 gem 'haml'
 gem 'thin'
 gem 'sass'
+gem 'susy'
--- lib/app.rb
+++ lib/app.rb
@@ -1,10 +1,22 @@
-require 'sinatra'
-require 'haml'
 require 'coffee_script'
+require 'compass'
+require 'haml'
+require 'sinatra'
+require 'susy'
 
 class Application < Sinatra::Base
-  set :root, File.dirname('..')
+  set :root, File.join(File.dirname(__FILE__), '..')
   set :public_folder, 'public'
+
+  configure do 
+    Compass.configuration do |config| 
+      config.project_path = root 
+      config.sass_dir = "#{root}/views" 
+    end 
+
+    set :haml, { :format => :html5 } 
+    set :sass, Compass.sass_engine_options 
+  end 
 
   get '/' do
     haml :index
